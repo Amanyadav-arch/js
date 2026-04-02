7600ebe8376772914f11eace7ffba87f1807a372
Amanyadav-arch <aman.128211@gmail.com>
2026-04-02 12:00:09 +0000

    varialbles

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "amanyadave@gmail.com"
+var accountPassoword = "12345"
+accountCity = "Jaipur"
+let accountstate
+
+/*
+prefer not use var 
+because of issue in block scope and functional scope
+*/ 
+
+// accountId = 2 
+
+accountEmail = "hcf@gmai.com"
+accountPassoword = "121243432"
+accountCity = "Bengaluru"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassoword, accountCity, accountstate])
