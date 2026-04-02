862abbc3e335cbde05eb81a300e098ad6a4fd10c
Amanyadav-arch <aman.128211@gmail.com>
2026-04-02 11:58:56 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello"); //we are using node js, not browser
+
+// code redaibility should be higher
+
+let name = "aman"
+let age = 18
+let isloggedIn = false
+
+// Integer => 2 to the power 58
+// bigint
+// float => 
+// string => collection of character
+// boolean => true, false
+// character
+// membership
+// null => stand along value
+// symbol => unique
+
+
+//object
+//typeof
+
+
+console.log(typeof null)
