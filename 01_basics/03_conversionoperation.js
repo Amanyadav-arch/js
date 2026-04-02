c16cd8da60d62aacf3110543bd7a6cb383960c17
Amanyadav-arch <aman.128211@gmail.com>
2026-04-02 17:14:32 +0000

    typeconversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+// let score = undefined
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)
+
+// let isLoggedIn = 1
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+// console.log(typeof booleanisLoggedIn)
+
+// 1 => true
+// 0 => false
+// empty string => false
+// string => true
+
+
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
