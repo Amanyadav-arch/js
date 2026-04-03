d81434574e26ce010380d693cf450176d7213e67
Amanyadav-arch <aman.128211@gmail.com>
2026-04-03 16:38:18 +0000

    backed

---
+++ 01_basics/04_comparisonOperator.js
@@ -0,0 +1,23 @@
+// type of comparison operators
+// console.log(2 == "2"); // true
+// console.log(2 === "2"); // false
+
+// == only checks the value but === checks the value and type both
+
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+// console.log(2 != 1)
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
