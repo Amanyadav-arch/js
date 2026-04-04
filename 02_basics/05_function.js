257d4b776257c09027e6d5a67d7c3127b5a18253
Amanyadav-arch <aman.128211@gmail.com>
2026-04-04 20:39:46 +0000

    fuction object

--- 02_basics/05_function.js
+++ 02_basics/05_function.js
@@ -33,4 +33,20 @@ function calculateCartTotal(...num1) {
     return num1
 }
 
-console.log(calculateCartTotal(5, 10)) // it will return an array containing the arguments passed to the function
+// console.log(calculateCartTotal(5, 10)) // it will return an array containing the arguments passed to the function
+
+let user = {
+    username : "aman",
+    score : 100,
+}
+
+function handleOject(anyObject) {
+    console.log(`username is ${anyObject.username} and score is ${anyObject.score}`) // it will log the username and score properties of the object passed as an argument
+}
+
+// handleOject(user) // it will log "aman" - the username property of the user object
+
+handleOject({
+    username : "john", 
+    score : 200
+})
