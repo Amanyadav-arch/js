8ee14c72f5dd64f5f7a7d2d57be877e151c98b52
Amanyadav-arch <aman.128211@gmail.com>
2026-04-05 12:52:15 +0000

    exephase

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -13,7 +13,7 @@
 ((name) => {
     //simple iife
     console.log("Db connection established") // it will log "Db connection established" - this code will run immediately after the function is defined
-})("aman")a
+})("aman")
 
 
 // iife is used to solve the problem of global scope pollution, it allows us to create a new scope for our variables and functions, so that they do not interfere with other variables and functions in the global scope. It is also used to execute code immediately after it is defined, without having to call the function separately.
